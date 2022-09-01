0c0768b5f47fa7c5ec0be743572f7ede10618e9c
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-09-01 15:05:46 -0500

    [ADD] - bcrypt was installed and the password is hashed in the db when saved

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto, UpdateUserDto } from '../dtos/users.dto';
 import { User } from '../entities/users.entity';
@@ -14,9 +15,16 @@ export class UsersService {
   }
 
   async create(data: CreateUserDto) {
-    const newModel = new this.userModel(data);
-    const model = await newModel.save();
-    return model;
+    const userModel = new this.userModel(data);
+    const passwordHashed = await bcrypt.hash(userModel.password, 10);
+    userModel.password = passwordHashed;
+    const modelSaved = await userModel.save();
+    const { password, ...rta } = modelSaved.toJSON();
+    return rta;
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email }).exec();
   }
 
   update(id: string, changes: UpdateUserDto) {
