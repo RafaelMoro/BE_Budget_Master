0c003fab180ef71807cf157e4d305c89fd12784d
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-02-03 16:23:12 -0600

    [ADD] - Connect mongoose with module and service

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AccountsController } from './controllers/accounts.controller';
+import { Account, AccountsSchema } from './entities/accounts.entity';
 import { AccountsService } from './services/accounts.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Account.name,
+        schema: AccountsSchema,
+      },
+    ]),
+  ],
   controllers: [AccountsController],
-  providers: [AccountsService]
+  providers: [AccountsService],
 })
 export class AccountsModule {}
--- src/accounts/services/accounts.service.ts
+++ src/accounts/services/accounts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Account } from '../entities/accounts.entity';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(
+    @InjectModel(Account.name) private accountModel: Model<Account>,
+  ) {}
+}
