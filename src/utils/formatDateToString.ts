0bca9cce56baafc3a407271de180cb3fa3ea8f4e
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-08-05 09:43:30 -0600

    [ADD] - Full date includes weekday, month, day and year

--- src/utils/formatDateToString.ts
+++ src/utils/formatDateToString.ts
@@ -1,11 +1,11 @@
 const MONTHS = [
   'Jan',
   'Feb',
-  'March',
-  'April',
+  'Mar',
+  'Apr',
   'May',
-  'June',
-  'July',
+  'Jun',
+  'Jul',
   'Aug',
   'Sept',
   'Oct',
@@ -13,13 +13,19 @@ const MONTHS = [
   'Dec',
 ];
 
+const WEEKDAY = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
 export const formatDateToString = (date: Date) => {
   const day = date.getDate();
+  const weekday = date.getDay();
   const month = date.getMonth();
   const hour = date.getHours();
+  const year = date.getFullYear();
+
   const hourString = String(hour).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const fullDate = `${MONTHS[month]} ${day}`;
+
+  const fullDate = `${WEEKDAY[weekday]}, ${MONTHS[month]} ${day}, ${year}`;
   const twelveHourPeriod = hour >= 0 && hour <= 11 ? 'am' : 'pm';
   const formattedTime = `${hourString}:${minutes}${twelveHourPeriod}`;
 
