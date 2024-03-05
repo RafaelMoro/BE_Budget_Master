a65ef19b9e4d1efd4a26364b7d042b8ad4586cf4
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-03-05 16:14:01 -0600

    Update lambda fn

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,17 +1,36 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GeneralAppExceptionFilter } from './exceptions/GeneralExceptionFilter.filter';
 
 let cachedServer: any;
 
-export const handler = async (event, context) => {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
-    cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
-    });
-  }
+export async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalFilters(new GeneralAppExceptionFilter());
+  await app.init();
 
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler = async (event, context) => {
+  cachedServer = cachedServer ?? (await bootstrap());
   return cachedServer(event, context);
+  // if (!cachedServer) {
+  //   const nestApp = await NestFactory.create(AppModule);
+  //   await nestApp.init();
+  //   cachedServer = serverlessExpress({
+  //     app: nestApp.getHttpAdapter().getInstance(),
+  //   });
+  // }
+
+  // return cachedServer(event, context);
 };
