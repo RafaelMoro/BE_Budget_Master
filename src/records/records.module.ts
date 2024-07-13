cef5471874b84a3e311600a5a32a983493c552ce
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-13 16:07:51 -0600

    Remove imports of records, income and expense model in records module

--- src/records/records.module.ts
+++ src/records/records.module.ts
@@ -1,34 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { RecordsController } from './controllers/records.controller';
-import { CreateIncome, IncomeSchema } from '../incomes/incomes.entity';
-import { AccountRecord, RecordsSchema } from './entities/records.entity';
 import { RecordsService } from './services/records.service';
 import { CategoriesModule } from '../categories/categories.module';
-import { CreateExpense, ExpenseSchema } from '../expenses/expenses.entity';
 import { ExpensesModule } from '../expenses/expenses.module';
 import { IncomesModule } from '../incomes/incomes.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      {
-        name: AccountRecord.name,
-        schema: RecordsSchema,
-      },
-      {
-        name: CreateExpense.name,
-        schema: ExpenseSchema,
-      },
-      {
-        name: CreateIncome.name,
-        schema: IncomeSchema,
-      },
-    ]),
-    CategoriesModule,
-    ExpensesModule,
-    IncomesModule,
-  ],
+  imports: [CategoriesModule, ExpensesModule, IncomesModule],
   controllers: [RecordsController],
   providers: [RecordsService],
   exports: [RecordsService],
