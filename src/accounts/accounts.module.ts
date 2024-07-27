ed2aa6fd712d3357a87617f415cea1df92422587
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-27 13:47:33 -0600

    Remove expense model from dependency of accounts

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -5,7 +5,6 @@ import { RecordsModule } from '../records/records.module';
 import { AccountsController } from './controllers/accounts.controller';
 import { Account, AccountsSchema } from './entities/accounts.entity';
 import { AccountsService } from './services/accounts.service';
-import { ExpensesModule } from 'src/expenses/expenses.module';
 import { IncomesModule } from 'src/incomes/incomes.module';
 
 @Module({
@@ -17,10 +16,10 @@ import { IncomesModule } from 'src/incomes/incomes.module';
       },
     ]),
     RecordsModule,
-    ExpensesModule,
     IncomesModule,
   ],
   controllers: [AccountsController],
   providers: [AccountsService],
+  exports: [AccountsService],
 })
 export class AccountsModule {}
