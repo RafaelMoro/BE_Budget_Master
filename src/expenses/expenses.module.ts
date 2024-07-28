63d09d489fdcf3ff5f57c1d622e6dff7814c5850
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-07-27 20:10:01 -0600

    Delete categories dependency from expenses module

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -3,7 +3,6 @@ import { ExpensesController } from './controllers/expenses.controller';
 import { ExpensesService } from './services/expenses.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CreateExpense, ExpenseSchema } from './expenses.entity';
-import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { CategoriesModule } from '../categories/categories.module';
         schema: ExpenseSchema,
       },
     ]),
-    CategoriesModule,
   ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
--- src/expenses/services/expenses.service.ts
+++ src/expenses/services/expenses.service.ts
@@ -7,7 +7,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { CreateExpense, Expense } from '../expenses.entity';
-import { CategoriesService } from '../../categories/services/categories.service';
 import {
   CreateExpenseDto,
   DeleteExpenseDto,
@@ -40,7 +39,6 @@ import { getMonthNumber } from '../../utils/getMonthNumber';
 export class ExpensesService {
   constructor(
     @InjectModel(CreateExpense.name) private expenseModel: Model<CreateExpense>,
-    private categoriesService: CategoriesService,
   ) {}
 
   async findExpenseById(expenseId: string) {
