17194e60058e4a9e1fa1b640c52009e0de576e9e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-01-25 13:08:01 -0600

    change full date format

--- src/utils/formatDateToString.ts
+++ src/utils/formatDateToString.ts
@@ -25,7 +25,7 @@ export const formatDateToString = (date: Date) => {
   const hourString = String(hour).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  const fullDate = `${WEEKDAY[weekday]}, ${day}-${MONTHS[month]}-${year}`;
+  const fullDate = `${WEEKDAY[weekday]}, ${day} ${MONTHS[month]}, ${year}`;
   const twelveHourPeriod = hour >= 0 && hour <= 11 ? 'am' : 'pm';
   const formattedTime = `${hourString}:${minutes}${twelveHourPeriod}`;
 
