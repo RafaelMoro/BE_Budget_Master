4c6d7a47c21b04c1a941213707df83f998fc82c9
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-02-07 17:31:01 -0600

    add createCheckoutSession fn

--- src/domain/payment/services/payment.service.ts
+++ src/domain/payment/services/payment.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from '@/config';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+
+  async createCheckoutSession() {
+    const stripe = new Stripe(this.configService.stripeApiKey);
+  }
+}
