e9061c08fcc6edcef58adeadfd27387f04fbec3a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-08 12:28:09 -0600

    add test price id, price not found error and look up key

--- src/domain/payment/payment.constants.ts
+++ src/domain/payment/payment.constants.ts
@@ -1,7 +1,15 @@
 // Test price Id
 export const PRICE_ID_ONE_TIME_ANUAL = 'price_1QqGHdKQRcBa6IkHST9uWu2q';
 export const PRICE_ID_ONE_TIME_MONTLY = 'price_1QqGHBKQRcBa6IkHJFM1qVka';
+export const PRICE_ID_SUBCRIPTION_ANUAL = 'price_1Qq1TTKQRcBa6IkH2PexcxiH';
+export const PRICE_ID_SUBSCRIPTION_MONTHLY = 'price_1Qq1RXKQRcBa6IkHFIlvlKUp';
+
+// Test Lookup price key
+export const LOOK_UP_KEY_RECURRENT_ANNUAL = 'standard_recurrent_annual';
+export const LOOK_UP_KEY_RECURRENT_MONTHLY = 'standard_recurrent_monthly';
 
 // Errors
 export const INVALID_PERIODICITY_ERROR =
   'The periodicity value can be only monthly or annual';
+export const PRICE_NOT_FOUND_ERROR =
+  'The price was not found with the given look up key';
