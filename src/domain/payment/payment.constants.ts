60a7c0fd1c121a10757fb7783cb851c3cdfd199e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-08 10:39:12 -0600

    add invalid periocidity error and change price id annual test constant

--- src/domain/payment/payment.constants.ts
+++ src/domain/payment/payment.constants.ts
@@ -1,3 +1,7 @@
 // Test price Id
-export const PRICE_ID_ONE_TIME_ANUAL = 'price_1Qq1TTKQRcBa6IkH2PexcxiH';
+export const PRICE_ID_ONE_TIME_ANUAL = 'price_1QqGHdKQRcBa6IkHST9uWu2q';
 export const PRICE_ID_ONE_TIME_MONTLY = 'price_1QqGHBKQRcBa6IkHJFM1qVka';
+
+// Errors
+export const INVALID_PERIODICITY_ERROR =
+  'The periodicity value can be only monthly or annual';
