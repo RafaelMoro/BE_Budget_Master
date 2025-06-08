70fb6ce1b7d5f02f3c7aaa5e0e068505a3cd4c91
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-08 15:55:04 -0600

    add resend into mail service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,7 +5,9 @@ import config from '@/config';
 import { BadRequestException, Injectable, Inject } from '@nestjs/common';
 
 import { MailForgotPasswordDto } from './dtos/mail.dtos';
+import ResetPassword from '../../emails/ResetPassword';
 import { PROD_URI } from '@/constants';
+import React from 'react';
 
 @Injectable()
 export class MailService {
@@ -17,25 +19,14 @@ export class MailService {
   async sendUserForgotPasswordEmail(payload: MailForgotPasswordDto) {
     try {
       const resend = new Resend(this.configService.mailer.resendApiKey);
-      // const { oneTimeToken, email, firstName, lastName } = payload;
-      // const url = `${PROD_URI}/reset-password/${oneTimeToken}`;
+      const { oneTimeToken, email, firstName, lastName } = payload;
+      const url = `${PROD_URI}/reset-password/${oneTimeToken}`;
 
-      // Change email for name of the user.
-      // await this.mailerService.sendMail({
-      //   to: email,
-      //   subject: 'Reset Password Request',
-      //   template: './reset-password',
-      //   context: {
-      //     firstName,
-      //     lastName,
-      //     url,
-      //   },
-      // });
       resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'count.on.me.app.personal.finances@gmail.com',
-        subject: 'Hello World',
-        html: '<p>Congrats on sending your <strong>first email</strong>!</p>',
+        from: 'no-reply@updates.budget-master.space',
+        to: email,
+        subject: 'Recupera tu contraseña en Budget Master',
+        react: React.createElement(ResetPassword, { url, firstName, lastName }),
       });
     } catch (error) {
       throw new BadRequestException(error.message);
